ca7f0af8d586e02c1aac6ddf194ea0330063302d
Alexi <alwxs40@gmail.com>
2020-11-20 19:45:57 -0600

    calulator

--- src/app/multiplication/multiplication.spec.ts
+++ src/app/multiplication/multiplication.spec.ts
@@ -1,14 +1,13 @@
 import { multiplication } from "./multiplication"
 
 describe('multiplication unit tests', () => {
-    
-    
-    it('Should mult 2 * 2 = 4', () => {
+
+    it('Should mul 2 * 4 = 8', () => {
         // Pattern AAA (Arrange - Act - Assert)
 
         // Arrange
         let result = 0;
-        
+
         // Act
         result = multiplication(2, 4);
 
@@ -16,7 +15,7 @@ describe('multiplication unit tests', () => {
         expect(result).toBe(8);
     })
 
-    it('Should mult 0 * -2 = 0', () => {
+    it(' Should mul 0 * -2 = 0', () => {
         // Arrange
         let result = 0;
         // Act
@@ -25,7 +24,7 @@ describe('multiplication unit tests', () => {
         expect(result).toBe(0);
     })
 
-    it('Should mult 3.0 + 0.6 = 1.5', () => {
+    it(' Should mul 3.0 * 0.5 = 1.5', () => {
         // Arrange
         let result = 0;
         // Act
@@ -34,7 +33,7 @@ describe('multiplication unit tests', () => {
         expect(result).toBe(1.5);
     })
 
-    it('Should mult 2.0 + 1.5 = 3.0', () => {
+    it(' Should mul 2.0 + 1.5 = 3.0', () => {
         // Arrange
         let result = 0;
         // Act
